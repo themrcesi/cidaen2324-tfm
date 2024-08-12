69177c22d2417aaa8905b95f8098b75b69ad1d63
cgarcia <cgarcia@g-n.com>
2024-08-12 22:31:54 +0200

    Add requireemnts streamlit

--- src/streamlit_app/sections/4_locations.py
+++ src/streamlit_app/sections/4_locations.py
@@ -21,9 +21,9 @@ df = get_gold_locations()
 st.title("Vista a nivel de Localización!")
 
 st.markdown(
-    "*Debido a las limitaciones de la API de Wallapop y para limitar el alcance de este TFM, se optó por retringir la extracción de datos a una localización concreta, el centro de Madrid. Además, la propia API no es consistente en la respuesta para los valores de localización, dejando en muchos casos este campo sin informar.*"
+    "*Debido a las limitaciones de la API de Wallapop y para limitar el alcance de este TFM, se optó por restringir la extracción de datos a una localización concreta, el centro de Madrid. Además, la propia API no es consistente en la respuesta para los valores de localización, dejando en muchos casos este campo sin informar.*"
 )
 
 st.markdown(
-    "*A raíz de estas limitaciones, esta vista se deja preparada para posibles expansiones en el futuro donde se tengan datos de varias localizaciones, permitiendo así análisis más interesantes a nivel geográfico.*"
+    "*A raíz de estas limitaciones, esta vista se deja preparada a modo de placeholder para posibles expansiones en el futuro donde se tengan datos de varias localizaciones, permitiendo así análisis más interesantes a nivel geográfico.*"
 )
